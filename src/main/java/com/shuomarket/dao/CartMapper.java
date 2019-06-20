5e10c2335490757e502334532c686744a54b6795
Shawn Yang <yangshuo960502@foxmail.com>
2019-06-20 21:52:15 +0800

    first commit

---
+++ src/main/java/com/shuomarket/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.shuomarket.dao;
+
+import com.shuomarket.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
